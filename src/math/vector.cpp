5b37163292bac4072179aa19be9a49e7c81c6c97
biart <biart95@gmail.com>
2019-11-01 15:18:38 +0100

    Add explicit instantiation for the most common vector types

---
+++ src/math/vector.cpp
@@ -0,0 +1,10 @@
+#include "vector.h"
+
+
+
+template class clipp::math::Vector2<std::int32_t>;
+template class clipp::math::Vector2<std::uint32_t>;
+template class clipp::math::Vector2<std::int64_t>;
+template class clipp::math::Vector2<std::uint64_t>;
+template class clipp::math::Vector2<float>;
+template class clipp::math::Vector2<double>;
